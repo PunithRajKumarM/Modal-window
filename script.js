8e30269096cd1845f00eb5f53ff14e251dd86f9d
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2023-10-19 09:45:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let modal = document.querySelector(".modal");
+let showModal = document.querySelectorAll(".show-modal");
+let overlay = document.querySelector(".overlay");
+let closeBtn = document.querySelector(".close");
+
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener("click", openModal);
+}
+
+closeBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
