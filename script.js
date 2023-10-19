0edebf87a535347fc74cb7e6ce41ec0ab53e8797
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2023-10-19 11:08:30 +0530

    added comments

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 "use strict";
+
 let modal = document.querySelector(".modal");
 let showModal = document.querySelectorAll(".show-modal");
 let overlay = document.querySelector(".overlay");
 let closeBtn = document.querySelector(".close");
 
+//Opening the modal
 const openModal = () => {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 
+//Closing the modal
 const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
